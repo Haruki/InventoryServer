07f029c3f2ed9af761b4a01aceefe882debbd382
Harald Borsutzky <borsutzky@hansaluftbild.de>
2019-03-26 14:25:29 +0100

    start web api development

---
+++ src/main/java/com/pimpelkram/inventory/server/InventoryWebApi.java
@@ -0,0 +1,12 @@
+
+package com.pimpelkram.inventory.server;
+
+import spark.Request;
+import spark.Response;
+
+public class InventoryWebApi {
+
+    static String getItems(Request request, Response response) {
+        return "hallooo moep lalala";
+    }
+}
--- src/main/java/com/pimpelkram/inventory/server/Main.java
+++ src/main/java/com/pimpelkram/inventory/server/Main.java
@@ -11,6 +11,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        get("/hello", (req, res) -> "Hello World");
+        get("/hello", InventoryWebApi::getItems);
     }
 }
