99bde21c3df415eecebd3a0a271cadbe2014ffd6
Harald Borsutzky <borsutzky@hansaluftbild.de>
2019-04-15 17:02:59 +0200

    ImageUpload returns filename only. For use in 1.updateItem requests and 2.addImage requests and 3.deleteImage requests (2 and 3 not implemented yet)

--- src/main/java/com/pimpelkram/inventory/server/ImageUpload.java
+++ src/main/java/com/pimpelkram/inventory/server/ImageUpload.java
@@ -57,7 +57,7 @@ public class ImageUpload {
                     e.printStackTrace();
                 }
             }
-            return "/images/" + givenFileName;
+            return givenFileName;
         }
     }
 }
